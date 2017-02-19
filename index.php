dc2da484ddbc4ddd6432c7437a0fce5a6e2946ad
Jessica Hsu <hsujessica21@gmail.com>
2017-02-19 14:50:04 -0500

    Update index.php with current code
    transfer to AWS server attempt #1

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-<?php
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,72 +16,83 @@
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	
 	<link rel="stylesheet" href="index.css">
+	
 	<title>Control-F</title>
 </head>
 <body data-spy="scroll" data-target=".navbar" data-offset="50">
 	<div class="container-fluid">
-		<div class="row">
-			<nav class="navbar navbar-default navbar-fixed-top">
+		<nav class="navbar navbar-default navbar-fixed-top">
 			<div class="navbar-header">
-       			 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#controlFNav">
+			<a id="navLogo" class="navbar-brand" href="index.html">
+            	<img  src="img/IconWhite.png" class="d-inline-block align-top" alt="" style="width: 40%; margin:0;">
+          	</a>
+       			 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#theNav">
           			<span class="icon-bar"></span>
           			<span class="icon-bar"></span>
           			<span class="icon-bar"></span>                        
       			</button>
-      			<a id="title" class="navbar-brand" href="index.html">Control-F</a>
     		</div>
     		<div>
-      			<div class="collapse navbar-collapse" id="controlFNav">
+      			<div class="collapse navbar-collapse" id="theNav">
        			 	<ul class="nav navbar-nav">
          			</ul>
          			<ul class="nav navbar-nav navbar-right">
-         				<li><a id="" href="">About</a></li>
-         				<li><a id="" href="">Find a developer/non-profit</a></li>         				
-         				<li><a id="" href="">Login/Register</a></li>
-         				<li><a id="" href="">Contact</a></li>
+         				<li><a href="#description">What We Do</a></li>
+         				<li><a href="about.html">About Us</a></li>
+         				<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown">Login/Register<span class="caret"></span></a>
+         					<ul class="dropdown-menu">
+         						<li><a href="loginDev.html">Login - developers</a></li>
+         						<li><a href="loginComp.html">Login - nonprofits</a></li>
+         					</ul>
+         				</li>
+         				
          			</ul>
       			</div>
     		</div>
-    		</nav>
-		</div>
-
-	<!-- Comments:
-	Put some nav bar on top. 
-	swap #banner and #line.  DONE
-	multiple entry points for devs/non-profits to look up people really quickly
-	 -->
-		<div class="row"  id="banner">
+    	</nav>
+		<div class="row"  id="banner"></div>
+		<div class="row" id=title>
 			<div class="col-sm-12" id="overlay1">
 				<div id="landing-page-title">Control-F</div>
-				<p id="vision">Need technical services? Or do you need technical experience?</p>
-				<div id="mission">Our mission is to provide technical services for small business owners and nonprofits in need.</div>
+				<span>Finding a Freelancer or Gaining Experience Should Be Easy</span>
 			</div>
 		</div>
+		
 		<div class="row" id="line">
 			<div class="col-sm-12" id="small-line">
-				
-				<span>Digitizing every aspect of our lives.</span>
+				<p>What We Do</p>
 			</div>
 		</div>
 		<div class="row" id="description">
 			<div class="col-sm-12" id="overlay2">
-				<p> An advertisement-free, neutral marketplace to connect small business owners and nonprofits with technical developers willing to volunteer time, 
-				or developers who are willing to work for less
+				<p> Control-F is an advertisement-free, neutral marketplace to connect small business owners and nonprofits with technical developers willing to volunteer time, 
+				or developers who are willing to work for less. 
 				</p>
 			</div> 
 		</div>
-		<div class="row" id="logins">
-			<div id="landing-page-logins">Login/Register</div>
-			<div class="col-sm-6">
-				<div class="login-type">Developers</div>
-			</div>
-			<div class="col-sm-6">
-				<div class="login-type">Nonprofits</div>
-			</div>
-		</div>
+		
+		
 	</div>
+	
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script> /*for the fancy scrolling function*/
+$(document).ready(function() {		//do this when document has finished loading
+	$("#flip").click(function() {
+		$("#panel").slideToggle("slow");
+	});
+	
+	$('a').click(function() {
+	    var href = $.attr(this, 'href');
+	    $("html, body").animate({
+	        scrollTop: $(href).offset().top
+	    }, 1500, function () {
+	        window.location.hash = href;
+	    });
+	    return false;
+	});
+	
+});
+</script>
 </body>
-
-
 
 </html>
