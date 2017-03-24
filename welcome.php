88a7bb7681394e7a6bc289bfb295c9b9a33867e6
jhsu26 <hsujessica21@gmail.com>
2017-03-24 16:43:09 -0400

    redirect attempt 5

--- welcome.php
+++ welcome.php
@@ -14,12 +14,9 @@
 	<!-- Latest compiled JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	
-	<link rel="stylesheet" href="viewProfile.css">
+	<link rel="stylesheet" href="welcome.css">
 	<link rel='icon' href='img\icon.ico' type='image/x-icon'>
 
-	<link href="./hello/redirect.css" rel="stylesheet" />
-	<link href="hello/redirect.css" rel="stylesheet" />
-	
 	<title>Control-F</title>
 </head>
 <body data-spy="scroll" data-target=".navbar" data-offset="50">
@@ -49,20 +46,32 @@
       			</div>
     		</div>
     	</nav>
-    	<h1 style="padding-top:20%;">welcome page</h1>
+    	<h1 style="padding-top:15%;" id="banner"></h1>
 
-      <script>
+<script src="hello.all.js"></script>
+<script src="hello/hello.js"></script>
+<script>
 
+var userName= "";
+var userEmail ="";
+      
 hello.on('auth.login', function(auth) {
     hello(auth.network).api('/me').then(function(r) {               
         console.log("name = "+r.name);
         console.log("email = " + r.email);
+        userName = r.name;
+        userEmail = r.email;
     });
+});
+
+$( document ).ready(function() {
+	var banner = document.getElementById("banner");
+	banner.innerHTML = "Hi " + userName + "! Your email is " + userEmail + ".";
 });
 
 
 </script>
-    <script src="hello.all.js"></script>
+    
 	
 </body>
 </html>
