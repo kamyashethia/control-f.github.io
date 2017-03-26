000429afac1f8578234c2384835db478d3bcb729
Michelle Menzies <mmenzies@comcast.net>
2017-03-26 14:07:15 -0400

    Update postAd.php

--- postAd.php
+++ postAd.php
@@ -14,11 +14,18 @@
 
 	<!-- Latest compiled JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+	<!-- jQUERY -->
 	
-	<link rel="stylesheet" href="viewProfile.css">
-	<link rel='icon' href='img\icon.ico' type='image/x-icon'>
+	<link rel="stylesheet" href="postAd.css">
+
+	<!--<link rel='icon' href='img/icon.ico' type='image/x-icon'>-->
+
 	<title>Control-F</title>
 </head>
+
+<?php include "connectDB.php" ?>
+
 <body data-spy="scroll" data-target=".navbar" data-offset="50">
 	<div class="container-fluid">
 		<nav class="navbar navbar-default navbar-fixed-top">
@@ -44,7 +51,100 @@
       			</div>
     		</div>
     	</nav>
-    	<h1>Post ad page</h1>
 	</div>
+
+
+	<h1>Post an Advertisement</h1>
+
+	<div>
+  		<form action="/postAdAction.php">
+   			<label for="project" id="projtype">Project Type</label>
+	   			<select id="project" name="project">
+	   				<option value="Website/Web Application">Website/Web Application</option>
+	      			<option value="Mobile - Android Application">Mobile - Android Application</option>
+	      			<option value="Mobile - iOS Application">Mobile - iOS Application</option>
+	      			<option value="Social Media">Social Media</option>
+	      			<option value="Game Development">Game Development</option>
+	      			<option value="Graphic Design">Graphic Design</option>
+	      			<option value="IT Support">IT Support</option>
+	      		</select>
+
+	      	<br>
+
+	      	<label for="why">Please give a short description of the project:</label>
+   				<input type="text" id="why" name="whyproject" placeholder="Description">
+
+  			<br>
+
+	      	<label for="why">What is the purpose of this project?</label>
+   				<input type="text" id="purpose" name="whyproject" placeholder="This project will...">
+
+  			<br>
+
+    		<button class="btn" type="button" id="preview" onclick="p()">Preview</button>
+
+    		<button class="btn" type="submit" id="submit">
+    			<a href="viewProfile.php">
+    				<span class="action_box right">
+    				</span>
+    			</a>
+    			Submit
+    		</button>
+
+  		</form>
+	</div>
+
+	<div class = "row" id="result-table">
+		<div class="col-sm-6">
+			<div class="table-responsive">
+				<table class="table table-striped" id="profiles">
+					<thead><tr>
+						<th>Name</th>
+						<th>Type</th>
+						<th>Description</th>
+						<th>Purpose</th>
+						<th>Profile</th></tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td id="name"></td>
+							<td id="type"></td>
+							<td id="description"></td>
+							<td id="purp"></td>
+							<td id="profile"></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+
+	<script>
+		function p() {
+			var text=[]; //this is where we store the dev/comp option and the selected product type/company focus
+			var table = document.getElementById('result-table');
+			var menu = document.getElementById('preview');
+
+			var project = document.getElementById('project');
+			var project_selected = project.options[project.selectedIndex].value;
+			var why = document.getElementById('why');
+			var purpose = document.getElementById('purpose');
+
+			var cell1 = document.getElementById('name');
+			var cell2 = document.getElementById('type');
+			var cell3 = document.getElementById('description');
+			var cell4 = document.getElementById('purp');
+			var cell5 = document.getElementById('profile');
+			
+			cell2.innerHTML = project_selected;
+			cell3.innerHTML = why.value;
+			cell4.innerHTML = purpose.value;
+			 			
+			var preview = 'preview';
+			var submit = 'submit';
+			table.style.display = "block";
+		}
+	</script>
+
 </body>
 </html>
